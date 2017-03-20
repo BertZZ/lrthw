790ca00c8771a1aa3cee53babe1416d51e0db2b9
BertZZ <bertie.wooles@hotmail.co.uk>
2017-03-20 20:39:16 +0000

    Drill_01.rb

---
+++ ex01/drill_01.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I said "said" do not touch this.'
+puts "This is another line"
+# The # sign will cause the line to not be read as code and therefore not run. 
