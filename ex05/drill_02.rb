65f37938fcbf23c51aba7c409ef0fac43dff37ab
BertZZ <bertie.wooles@hotmail.co.uk>
2017-03-22 17:34:59 +0000

    Solutions to Chapter 5

---
+++ ex05/drill_02.rb
@@ -0,0 +1,19 @@
+name = 'Zed A. Shaw'
+age = 35 
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_to_cm = height * 2.54
+weight_to_kg = weight * 0.45
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
