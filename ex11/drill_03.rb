f2765cb4e14c05b5ccefd67fe13ce761700abb03
BertZZ <bertie.wooles@hotmail.co.uk>
2017-03-23 23:00:08 +0000

    Chapter 11

---
+++ ex11/drill_03.rb
@@ -0,0 +1,8 @@
+print "Where are you from?"
+from = gets.chomp
+print "Are you Male or Female?"
+sex = gets.chomp
+print "Do you code?"
+code = gets.chomp
+
+print "So you are a #{sex} from #{from} and you code? #{code}"
